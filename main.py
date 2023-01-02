a10423316d30ecc16e2acdbc81963ae00a0c26b5
Kseniya-la11 <lsg1971@mail.ru>
2023-01-02 15:56:33 +0400

    Initial commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = int(input("Введите сумму:"))
 float_percent = list(per_cent.values())
 #находим сумму по каждому отдельному банку
-deposit = [money * i for i in float_percent]
+dep_osit = [money * i for i in float_percent]
+#формируем список deposit значений
+deposit = [int(dep_osit) for dep_osit in dep_osit]
 #находим максимальную сумму
 deposit_max = max(deposit)
-#переводим в целое число
-dep_max = int(float(deposit_max))
-print("Максимальная сумма, которую вы можете заработать -", dep_max)
+print(deposit)
+print("Максимальная сумма, которую Вы можете заработать -", deposit_max)
 
